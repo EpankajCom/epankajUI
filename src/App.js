51d0716292962d163cc6cb462b7dd793875460eb
priyanshu <imraj383foru@gmail.com>
2024-04-16 12:41:10 +0530

    Hello component is designed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-
+import HelloMessage from './component/hello';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Welcome to Rajlee Innovations</h1>
+        <HelloMessage />
       </header>
     </div>
   );
