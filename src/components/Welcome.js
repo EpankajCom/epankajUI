51d3f8286d5d20b59929acb0cd852820cc593cea
Rishikeshhhh <rishikesh20032000.rishikesh@ug.cusat.ac.in>
2024-04-17 12:20:26 +0530

    welcome by rishikesh

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function Welcome() {
-  return <div>This is the Welcome page.</div>;
-}
+  return 
+  <div>
+  <h1>welcome by rishikesh</h1>;
+  <h2>This is the Welcome page.</h2>;
+  </div>
+  }
 
 export default Welcome;
