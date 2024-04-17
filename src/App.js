e5ffc7c3c7cbfc3995b7bd6c70d3a703424a7d4f
Pankaj Kumar <pankajsoe44@gmail.com>
2024-04-17 12:00:10 +0530

    Base Project Details

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import './App.css';
-import HelloMessage from './component/hello';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
+// Import your components
+import Home from './components/Home';
+import Login from './components/Login';
+import Welcome from './components/Welcome';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to Rajlee Innovations</h1>
-        <HelloMessage />
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/welcome">Welcome</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/welcome" element={<Welcome />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Login() {
+  return <div>
+    <h1>This is the Login page.</h1>
+    <h1>Welcome to Login Page</h1>
+    </div>;
+}
+
+export default Login;
