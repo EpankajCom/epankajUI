51eb63b59b9a7c6d3edc805d871e861532aaad40
priyanshu <imraj383foru@gmail.com>
2024-04-17 13:40:14 +0530

    Priyanshu Footer

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import './App.css';
 import React from 'react';
 import HelloMessage from './component/hello';
 import Hero from './component/Heros';
-
+import FooterLast from './component/footerlast';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       <Hero/>
+      
         <h1>Welcome to Rajlee Innovations</h1>
         <HelloMessage />
+        <FooterLast/>
       </header>
     </div>
+    
   );
 }
 
